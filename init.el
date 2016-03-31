914276cc002e4b239cbc2715a50f308f3a6328f2
Sean Heath <heath@VXI.local>
2016-03-31 12:14:22 -0400

    updated a bunch of plugins and default start up options

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
-(defvar required-packages '(color-theme-sanityinc-tomorrow smart-mode-line evil-easymotion evil-org evil-leader evil-smartparens evil-tabs evil org-download org-pomodoro smartparens) "list of packages to install at launch")
+(defvar required-packages '(auto-complete linum-relative sublime-themes fill-column-indicator color-theme-sanityinc-tomorrow smart-mode-line evil-easymotion evil-org evil-smartparens evil-tabs evil org-download org-pomodoro smartparens) "list of packages to install at launch")
 
 (require 'cl)
 ; method to check if all packages are installed
@@ -34,7 +34,7 @@
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(org-agenda-files (quote ("~/Sync/org/work.org")))
+ '(org-agenda-files (quote ("~/Sync/org/personal.org" "~/Sync/org/work.org")))
  '(package-selected-packages
    (quote
     (smart-mode-line-powerline-theme org-download evil evil-easymotion))))
@@ -45,12 +45,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; evil-leader
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader ",")
-;; evil-org
-(require 'evil-org)
 ;; evil mode!!!
 (require 'evil)
 (evil-mode 1)
@@ -61,14 +55,27 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-directory "~/Sync/org/")
-;; org-mobile
-(setq org-mobile-directory "~/Sync/org/mobileorg/")
-(setq org-mobile-inbox-for-pull "~/Sync/org/inbox.org")
-(setq org-mobile-files "~/Sync/org/")
-(setq org-mobile-checksum-binary "~/Sync/apps/sha1sum.exe")
+(setq org-log-done 'time)
+
 ;; org-download
 (require 'org-download)
 (setq-default org-download-image-dir "~/Sync/org/images")
+
 ;; org-pomodoro
 (require 'org-pomodoro)
 (global-set-key (kbd "C-c p") 'org-pomodoro)
+
+;; Fill column indicator
+(require 'fill-column-indicator)
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+
+;; Show relative line numbers
+(require 'linum-relative)
+
+;; Enable auto-complete
+(ac-config-default)
+
+;; Startup stuff
+(setq inhibit-splash-screen t)
+(find-file "~/Sync/org/personal.org")
